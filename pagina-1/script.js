edcd32946887b9b84f6e631d4fb20870289c5c10
Oscarmps <oscarmezaperezxd@gmail.com>
2024-01-09 05:51:33 -0600

    ultimo commit

---
+++ pagina-1/script.js
@@ -0,0 +1,11 @@
+function ir(){
+    var c=12345;
+    var u="oscarmezaperezxd@gmail.com";
+    if(document.form.password.value==c && document.form.login.value==u){
+     alert("Bienvenidos al portal de citas")
+    window.location="index-2.html";
+    } 
+    else{
+    alert("por favor ingrese, correo y nip correcto.");
+   } 
+}
